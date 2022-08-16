aedb996a2242a37e9a9018ddabeeb8bb5f0e6eca
Lemonidas <leonkala8@gmail.com>
2022-08-16 15:11:54 +0300

    Initial commit

--- index.js
+++ index.js
@@ -24,7 +24,33 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date?", function (req, res) {
+  let input = req.params.date;
+  let timestamp = parseInt(input);
+  let dateFormat = new Date(input);
+  let currentTime = new Date();
+  
+  if (dateFormat != 'Invalid Date') {
+    res.json({
+      unix: dateFormat.getTime(),
+      utc: dateFormat.toUTCString()
+    });
+  } else if (timestamp == Number(timestamp)) {
+    res.json({
+      unix: timestamp,
+      utc: new Date(timestamp).toUTCString()
+    });
+  } else if (input == undefined) {
+    res.json({
+      unix: currentTime.getTime(),
+      utc: currentTime.toUTCString()
+    });
+  } else {
+    res.json({
+      error: 'Invalid Date'
+    });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
